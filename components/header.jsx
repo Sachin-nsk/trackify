d148e9bba57a8f92daabbc30fddb34056029ddbc
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2025-05-27 19:12:35 +0530

    Add authentication - SignIn & SignUp Page

---
+++ components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
+
+const header = () => {
+  return (
+    <div className='fixed top-0'>
+        <SignedOut>
+            <SignInButton />
+        </SignedOut>
+        <SignedIn>
+            <UserButton />
+        </SignedIn>
+    </div>
+  )
+}
+
+export default header
