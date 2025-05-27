c9926135dcabd547403957d82b3e30474985e4a9
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2025-05-27 16:42:41 +0530

    Change to webpack due to potential errors using turbopack

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
 		"./app/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 		"./lib/**/*.{js,ts,jsx,tsx}",
-		"./**/*.{js,ts,jsx,tsx,css,html}"
+		
 	],
   theme: {
   	extend: {
